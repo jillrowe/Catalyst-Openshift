57b0dbbcf56a1a04a4d91f23575ab0864c94af2e
Jillian Rowe <jir2004@qatar-med.cornell.edu>
2013-09-12 11:45:58 +0300

    trying without proxy

--- MyApp/lib/MyApp.pm
+++ MyApp/lib/MyApp.pm
@@ -31,9 +31,9 @@ extends 'Catalyst';
 __PACKAGE__->config->{using_frontend_proxy} = 1;
 __PACKAGE__->config->{"X-Forwarded-Port"} = 443;
 
- __PACKAGE__->apply_request_class_roles(qw/
-    Catalyst::TraitFor::Request::ProxyBase
-/);
+# __PACKAGE__->apply_request_class_roles(qw/
+#    Catalyst::TraitFor::Request::ProxyBase
+#/);
 
 our $VERSION = '0.01';
 
